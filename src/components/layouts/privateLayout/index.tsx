c7fe9a8a8e42dda9e0518cecc10f45784a2f0846
Hieng55 <bosshieng55@gmail.com>
2024-03-07 16:52:33 +0700

    update font

--- src/components/layouts/privateLayout/index.tsx
+++ src/components/layouts/privateLayout/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { Quicksand } from "next/font/google";
+const quicksand = Quicksand({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className={quicksand.className}>
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
