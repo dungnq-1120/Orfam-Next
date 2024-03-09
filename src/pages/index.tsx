653063f1d566490f6ce2cff0a9b9ba7c65240cc9
Hieng55 <bosshieng55@gmail.com>
2024-03-09 09:20:51 +0700

    feature.function_common update card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Layout from "@/components/layouts/privateLayout";
-import { Checkbox } from "@/shared/checkbox";
-import Card from "@/shared/card";
-
 import type { ReactElement } from "react";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+    </>
+  );
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
--- src/shared/card/index.tsx
+++ src/shared/card/index.tsx
@@ -1,43 +1,75 @@
-import Image from "next/image";
-import React from "react";
-import chicken from "../../../public/image/products30-min.jpg";
-import { Button } from "../button";
+import * as React from "react"
+import { cn } from "@/lib/utils"
 
-const Card = () => {
-  return (
-    <>
-      <div className="card w-1/5 cursor-pointer relative group">
-        <div className="image overflow-hidden h-60">
-          <Image src={chicken} alt="product" className="w-full" />
-        </div>
-        <div className="content bg-slate-200 p-2">
-          <span className="text-gray-500 text-xs">hi</span>
-          <h5 className="text-ctBlue7 text-sm font-medium mt-2 mb-2">gà xòa</h5>
-          <h4>
-            <span className="text-red-600 font-semibold mr-2">$500</span>
-            <del className="text-slate-500 text-sm">$200</del>
-          </h4>
-          <div></div>
-        </div>
-        <div className="detail opacity-0 group-hover:opacity-100 z-10 absolute top-0 left-0 bg-slate-100 w-full h-full group flex justify-center items-center text-blue-ct5 transition duration-500">
-          <div className="translate-y-2 group-hover:translate-y-0 duration-500">
-            <ul>
-              <li>Type: Organic</li>
-              <li>MFG: August 4 2024</li>
-              <li>LIFE: 60</li>
-            </ul>
-            <Button className="group-hover:bg-green-ct5 mt-2 px-10 py-3 rounded duration-500">ADD CART</Button>
-          </div>
-        </div>
-        <div
-          className="sale bg-red-500 text-white z-0 absolute top-1
-        left-1 p-1 px-2 rounded"
-        >
-          50%
-        </div>
-      </div>
-    </>
-  );
-};
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-xl border bg-card text-card-foreground shadow",
+      className
+    )}
+    {...props}
+  />
+))
+Card.displayName = "Card"
 
-export default Card;
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5", className)}
+    {...props}
+  />
+))
+CardHeader.displayName = "CardHeader"
+
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn("font-semibold leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+CardTitle.displayName = "CardTitle"
+
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+CardDescription.displayName = "CardDescription"
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("pt-0", className)} {...props} />
+))
+CardContent.displayName = "CardContent"
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center pt-0", className)}
+    {...props}
+  />
+))
+CardFooter.displayName = "CardFooter"
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
