8769b5bcae7d6b6cd49060466a957fe4f4810bf9
Hieng55 <bosshieng55@gmail.com>
2024-03-21 00:53:55 +0700

    Feature.shop: responsive cartdetail page

--- src/components/features/cart/productDetail/description/index.tsx
+++ src/components/features/cart/productDetail/description/index.tsx
@@ -13,19 +13,23 @@ const Description = () => {
     { label: "PRODUCT DESCRIPTION", value: "PRODUCT-DESCRIPTION" },
     { label: "ADDITIONAL INFORMATION", value: "ADDITIONAL-INFORMATION" },
   ];
-  
+
   return (
     <div className="bg-white my-8 shadow-2xl py-8 px-5 rounded-lg">
       <div className="tabs">
-        <Tabs defaultValue="PRODUCT-DESCRIPTION" className="w-full m-auto">
-          <div className="flex justify-center items-center mb-6 ">
-            <TabsList className="w-full gap-5 relative border-b-1 rounded-none py-6 xl:w-3/5 nm:w-4/5 sm:bg-transparent md:!w-full md:bg md:mb-10">
+        <Tabs defaultValue="PRODUCT-DESCRIPTION" className="w-full m-auto ">
+          <div className="flex justify-center items-center mb-6 sm:mb-10">
+            <TabsList
+              className="w-full gap-5 relative border-b-1 rounded-none py-6 
+            xl:w-3/5 nm:w-4/5 sm:bg-transparent 
+            md:!w-full md:bg md:mb-10 sm:p-0 sm:h-12 sm:border-none sm:mb-2"
+            >
               {tabData.map((tab) => (
                 <TabsTrigger
                   key={tab.value}
-                  className="after:absolute inline-block px-0 after:left-0 py-3 after:-bottom-1 after:w-full after:h-1 data-[state=active]:after:bg-green-ct5 -translate-y-2/4 md:rounded-none sm:-translate-y-0 font-semibold text-blue-ct7
+                  className="after:absolute xs:text-xs inline-block px-0 after:left-0 py-3 after:-bottom-1 after:w-full after:h-1 data-[state=active]:after:bg-green-ct5 -translate-y-2/4 md:rounded-none sm:-translate-y-0 font-semibold text-blue-ct7
                    data-[state=active]:bg-white
-                   data-[state=active]:text-green-ct5 data-[state=active]:shadow-none"
+                   data-[state=active]:text-green-ct5 data-[state=active]:shadow-none sm:after:hidden sm:text-white sm:w-full sm:bg-blue-ct7 sm:data-[state=active]:bg-green-ct5 sm:data-[state=active]:text-white"
                   value={tab.value}
                 >
                   {tab.label}
@@ -33,27 +37,31 @@ const Description = () => {
               ))}
             </TabsList>
           </div>
-      
-            <TabsContent key="PRODUCT-DESCRIPTION" value="PRODUCT-DESCRIPTION">
-              <div className="content-des">
-                <h4 className="text-sm font-bold text-blue-ct7">PRODUCT DETAILS</h4>
-                <p className="my-4 text-blue-ct7 text-sm font-medium">
-                  Apricots Form is an armless modern chair with a minimalistic expression. With a simple and contemporary design Form Chair has a soft
-                  and welcoming ilhouette and a distinctly residential look. The legs appear almost as if they are growing out of the shell. This
-                  gives the design flexibility and makes it possible to vary the frame. Unika is a mouth blown series of small, glass pendant lamps,
-                  originally designed for the Restaurant Gronbech. Est eum itaque maiores qui blanditiis architecto. Eligendi saepe rem ut. Cumque
-                  quia earum eligendi.
-                </p>
-                <Image className="w-full rounded-xl mt-4" src={productDes} alt="" />
-              </div>
-            </TabsContent>
-             <TabsContent key="ADDITIONAL-INFORMATION" value="ADDITIONAL-INFORMATION">
-             <div className="content-des">
-             <p className="my-4 text-blue-ct7 text-sm font-medium">
-               Designed by Puik in 1949 as one of the first models created especially for Carl Hansen & Son, and produced since 1950. The last of a series of chairs wegner designed based on inspiration from antique chinese armchairs. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia eserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, aque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-               </p>
-               <h4 className="text-sm font-bold text-blue-ct7">PRODUCT DETAILS</h4>
-              <ul className="text-blue-ct7 text-sm mt-4">
+
+          <TabsContent key="PRODUCT-DESCRIPTION" value="PRODUCT-DESCRIPTION">
+            <div className="content-des">
+              <h4 className="text-sm font-bold text-blue-ct7">PRODUCT DETAILS</h4>
+              <p className="my-4 text-blue-ct7 text-sm font-medium xs:text-xs">
+                Apricots Form is an armless modern chair with a minimalistic expression. With a simple and contemporary design Form Chair has a soft
+                and welcoming ilhouette and a distinctly residential look. The legs appear almost as if they are growing out of the shell. This gives
+                the design flexibility and makes it possible to vary the frame. Unika is a mouth blown series of small, glass pendant lamps,
+                originally designed for the Restaurant Gronbech. Est eum itaque maiores qui blanditiis architecto. Eligendi saepe rem ut. Cumque quia
+                earum eligendi.
+              </p>
+              <Image className="w-full rounded-xl mt-4" src={productDes} alt="" />
+            </div>
+          </TabsContent>
+          <TabsContent key="ADDITIONAL-INFORMATION" value="ADDITIONAL-INFORMATION">
+            <div className="content-des">
+              <p className="my-4 text-blue-ct7 text-sm font-medium xs:text-xs">
+                Designed by Puik in 1949 as one of the first models created especially for Carl Hansen & Son, and produced since 1950. The last of a
+                series of chairs wegner designed based on inspiration from antique chinese armchairs. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia eserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, aque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+                dicta sunt explicabo.
+              </p>
+              <h4 className="text-sm font-bold text-blue-ct7">PRODUCT DETAILS</h4>
+              <ul className="text-blue-ct7 text-sm mt-4 xs:text-xs">
                 <li>Material: Plastic, Wood</li>
                 <li>Legs: Lacquered oak and black painted oak</li>
                 <li>Length: 48cm</li>
@@ -61,9 +69,8 @@ const Description = () => {
                 <li>Weight: 1lb</li>
                 <li>Color: White</li>
               </ul>
-             </div>
-           </TabsContent>
- 
+            </div>
+          </TabsContent>
         </Tabs>
       </div>
     </div>
--- src/components/features/cart/productDetail/infoProduct/index.tsx
+++ src/components/features/cart/productDetail/infoProduct/index.tsx
@@ -10,8 +10,8 @@ const InfoProduct = () => {
     <div className="info-product gap-2 w-full">
       <div className=" shadow-xl p-5">
         <div className="py-5 inline-block">
-          <h3 className="font-semibold text-2xl text-blue-ct7">Guava Leaves From Trees Grown In South</h3>
-          <ul className="flex justify-between mt-3 text-sm text-blue-ct7 font-medium">
+          <h3 className="font-semibold text-2xl text-blue-ct7 xs:text-xl">Guava Leaves From Trees Grown In South</h3>
+          <ul className="flex justify-between mt-3 text-sm text-blue-ct7 font-medium xs:text-xs">
             <li>
               Brands: <span className="text-green-ct5 font-semibold">Super Market</span>
             </li>
@@ -20,31 +20,31 @@ const InfoProduct = () => {
             </li>
           </ul>
         </div>
-        <div className="product-content-detail border-t-1 flex gap-5 pt-6">
+        <div className="product-content-detail border-t-1 flex gap-5 pt-6 nm:items-center csm:block">
           <div className="overflow-hidden w-full h-full">
-            <Image src={cherry} alt="" className="w-full h-full object-cover" />
+            <Image src={cherry} alt="" className="w-full h-full object-cover csm:w-2/4 csm:h-2/4 csm:m-auto xs:w-full xs:h-full" />
           </div>
           <div className="detail w-full">
-            <h3 className="text-red-600 font-semibold text-2xl">$ 35.00</h3>
-            <ul className="list-disc text-blue-ct6 text-sm font-medium pl-5 mt-3 pb-4 border-b-1">
+            <h3 className="text-red-600 font-semibold text-2xl xs:text-xl">$ 35.00</h3>
+            <ul className="list-disc text-blue-ct6 text-sm font-medium pl-5 mt-3 pb-4 border-b-1 xs:text-xs">
               <li>Delicious non - dairy cheese sauce</li>
               <li className="my-1">Vegan & Allergy friendly</li>
               <li>Smooth, velvety dairy free cheese sauce</li>
             </ul>
-            <div className="quantity-product flex items-center gap-5 py-6 border-b-1">
-              <span className="text-sm font-semibold text-blue-ct7">QTY:</span>
-              <div className="inline-flex items-center bg-gray-100 rounded-3xl py-1 w-28">
+            <div className="quantity-product flex items-center gap-5 py-6 border-b-1 xs:block">
+              <span className="text-sm font-semibold text-blue-ct7 xs:block xs:text-center">QTY:</span>
+              <div className="inline-flex items-center bg-gray-100 rounded-3xl py-1 w-28 xs:!flex xs:w-full xs:mt-4">
                 <Button className="text-blue-ct7 px-0 py-0 w-full h-full flex justify-center items-center text-4xl leading-none bg-transparent">
                   -
                 </Button>
                 <InputForm className="text-blue-ct7 p-0 font-semibold w-full text-center h-full border-0 bg-transparent" value="1" />
                 <Button className="text-blue-ct7 px-0 py-0 w-full h-full flex justify-center items-center text-2xl bg-transparent">+</Button>
               </div>
-              <Button types="success" className="py-3 px-12 rounded-3xl">
+              <Button types="success" className="py-3 px-12 rounded-3xl xs:w-full xs:mt-4">
                 ADD TO CART
               </Button>
             </div>
-            <ul className="pt-6 text-sm font-medium text-blue-ct7">
+            <ul className="pt-6 text-sm font-medium text-blue-ct7 xs:text-xs">
               <li>
                 Availability: <span className="text-green-400 font-semibold">50 In stock</span>
               </li>
@@ -52,8 +52,8 @@ const InfoProduct = () => {
               <li>Tags: Apricots, Fresh</li>
             </ul>
             <div className="bg-gray-200 text-center py-4 rounded-lg mt-5">
-              <Image className="m-auto" src={payments} alt="" />
-              <span className="text-blue-ct7 font-medium text-sm mt-3 block">Guarantee safe & Secure checkout</span>
+              <Image className="m-auto px-3" src={payments} alt="" />
+              <span className="text-blue-ct7 font-medium text-sm mt-3 block xs:text-xs">Guarantee safe & Secure checkout</span>
             </div>
           </div>
         </div>
