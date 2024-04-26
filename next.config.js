4aaf3a3bd3a71b973cc01e4af684517cef4fb7f9
dungnq-1120 <nguyen.quoc.dung@sun-asterisk.com>
2024-04-26 14:28:52 +0700

    remove package.log

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["agricola.axiomthemes.com", "media.istockphoto.com", "html.hixstudio.net", "ninzio.com", "ninetheme.com", "bubulla.like-themes.com"],
+    domains: [
+      "agricola.axiomthemes.com",
+      "media.istockphoto.com",
+      "html.hixstudio.net",
+      "ninzio.com",
+      "ninetheme.com",
+      "bubulla.like-themes.com",
+    ],
   },
+  distDir: "build",
 };
